3cde6d81508ad1bf055e0fa7a2ade269028447f8
vanvixi <vanvixi.dev@gmail.com>
2024-06-06 20:11:11 +0700

    ✨ (main.dart):  - Introduce new features to the app by adding new UI components and updating the layout design.  - Refactor code to improve readability and maintainability by initializing minExtent and maxExtent variables in the build method instead of didChangeDependencies method.  - Refactor SliverAppBarDelegate class to improve code readability and maintainability by restructuring the build method and optimizing widget positioning and styling.  - Refactor RectClipper class to improve code readability and maintainability

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_momo_ui/action_and_balance_card.dart';
+import 'package:flutter_momo_ui/balance_bar.dart';
+import 'package:flutter_momo_ui/icon_img_button.dart';
 import 'package:flutter_momo_ui/notification_handler.dart';
 
 void main() {
@@ -41,18 +44,12 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   late double minExtent;
+  late double maxExtent;
   final ScrollController scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
-  }
-
-  @override
-  void didChangeDependencies() {
-    minExtent = kToolbarHeight + MediaQuery.paddingOf(context).top;
-
-    super.didChangeDependencies();
   }
 
   @override
@@ -63,10 +60,15 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    minExtent = kToolbarHeight + MediaQuery.paddingOf(context).top;
+    maxExtent = Platform.isAndroid ? 216 : 256;
+
     return Scaffold(
       extendBodyBehindAppBar: true,
+      backgroundColor: Colors.white,
       body: AppBarScrollHandler(
         minExtent: minExtent,
+        maxExtent: maxExtent,
         controller: scrollController,
         child: CustomScrollView(
           controller: scrollController,
@@ -75,19 +77,17 @@ class _MyHomePageState extends State<MyHomePage> {
               pinned: true,
               delegate: SliverAppBarDelegate(
                 minExtent: minExtent,
-                maxExtent: Platform.isAndroid ? 200 : 230,
+                maxExtent: maxExtent,
               ),
             ),
             SliverList.list(
               children: List<Widget>.generate(
                 20,
                 (int index) => Card(
+                  margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
                   child: Container(
                     alignment: Alignment.center,
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 24,
-                      horizontal: 16,
-                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
                     child: Text(index.toString()),
                   ),
                 ),
@@ -134,207 +134,268 @@ class SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
 
   @override
   Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return LayoutBuilder(
-      builder: (BuildContext context, BoxConstraints constraints) {
-        final currentExtent = max(minExtent, maxExtent - shrinkOffset);
-        // 0.0 -> Expanded
-        // 1.0 -> Collapsed
-        double t = clampDouble(1.0 - (currentExtent - minExtent) / deltaExtent, 0, 1);
-        CollapsingNotification(t).dispatch(context);
+    final currentExtent = max(minExtent, maxExtent - shrinkOffset);
+    // 0.0 -> Expanded
+    // 1.0 -> Collapsed
+    double t = clampDouble(1.0 - (currentExtent - minExtent) / deltaExtent, 0, 1);
+    CollapsingNotification(t).dispatch(context);
 
-        final List<Widget> children = <Widget>[];
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            final List<Widget> children = <Widget>[];
+            final splashColoredBox = ColoredBox(color: transformColor(null, Colors.white, t, 3));
 
-        double height = maxExtent;
+            double imgBgrHeight = maxExtent;
 
-        // Background image
-        if (constraints.maxHeight > height) height = constraints.maxHeight;
-        children.add(Positioned(
-          top: 0,
-          left: 0,
-          right: 0,
-          height: height - deltaExtent / 2,
-          child: imgBgr,
-        ));
+            // Background image
+            if (constraints.maxHeight > imgBgrHeight) imgBgrHeight = constraints.maxHeight;
+            children
+              ..add(Positioned(
+                top: 0,
+                left: 0,
+                right: 0,
+                height: imgBgrHeight - deltaExtent / 2,
+                child: imgBgr,
+              ))
 
-        // Box
-        final boxPaddingBottom = Platform.isAndroid ? 16.0 : 12.0;
-        const actionFloatTextStyle = TextStyle(
-          color: Colors.black,
-          fontWeight: FontWeight.w600,
-          height: 1.1,
-          fontSize: 14,
-        );
-        children
-          ..add(Positioned(
-            left: 0,
-            right: 0,
-            bottom: 0,
-            child: Container(
-              height: deltaExtent,
-              alignment: Alignment.bottomCenter,
-              margin: EdgeInsets.symmetric(horizontal: transform(16, 0, t, 2)),
-              padding: EdgeInsets.only(bottom: boxPaddingBottom),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(transform(8, 0, t, 2)),
-              ),
-              child: const DefaultTextStyle(
-                style: actionFloatTextStyle,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                      child: Text(
-                        'Nap/Rut',
-                        textAlign: TextAlign.center,
+              // Splash transform color bottom
+              ..add(Positioned(
+                bottom: 0,
+                width: constraints.maxWidth,
+                height: deltaExtent,
+                child: splashColoredBox,
+              ));
+
+            // Card
+            const double cardPadding = 8;
+            const double cardMarginHorizontal = 16;
+            children
+              ..add(
+                Positioned(
+                  left: cardMarginHorizontal,
+                  right: cardMarginHorizontal,
+                  bottom: 0,
+                  height: deltaExtent,
+                  child: ActionAndOverviewInfoCard(
+                    contentPadding: const EdgeInsets.all(cardPadding),
+                    borderRadius: BorderRadius.circular(transform(12, 0, t, 2)),
+                  ),
+                ),
+              )
+
+              // Background image Clipped
+              ..add(Positioned(
+                top: 0,
+                height: imgBgrHeight - deltaExtent / 2,
+                width: constraints.maxWidth,
+                child: ClipRect(
+                  clipper: RectClipper(minExtent),
+                  child: imgBgr,
+                ),
+              ))
+
+              // Splash transform color top
+              ..add(Positioned(
+                top: 0,
+                height: minExtent,
+                width: constraints.maxWidth,
+                child: splashColoredBox,
+              ));
+
+            // App bar
+            const appBarPadding = SizedBox(width: 8);
+            final appBarContentWidth = constraints.maxWidth - (appBarPadding.width! * 2);
+            const totalIconImgButtonSize = IconImgButton.tapTargetSize * 7;
+            final appBarSpace = SizedBox(width: (appBarContentWidth - totalIconImgButtonSize) / 6);
+
+            //App bar fixed position
+            Color iconBgrColor = transformColor(Colors.black54, null, t, 4);
+            children.add(Positioned(
+              left: 0,
+              top: 0,
+              right: 0,
+              child: Container(
+                height: minExtent,
+                color: transformColor(null, const Color(0xff395241), t, 2),
+                child: SafeArea(
+                  bottom: false,
+                  child: Row(
+                    children: [
+                      appBarPadding,
+                      SearchArea(
+                        appBarContentWidth: appBarContentWidth,
+                        appBarSpace: appBarSpace,
+                        iconBgrColor: iconBgrColor,
                       ),
-                    ),
-                    SizedBox(width: 16),
-                    Expanded(
-                      child: Text(
-                        'Nhan tien',
-                        textAlign: TextAlign.center,
+                      appBarSpace,
+                      const Spacer(),
+                      appBarSpace,
+                      IconImgButton(
+                        'notifications_bell.webp',
+                        backgroundColor: iconBgrColor,
                       ),
-                    ),
-                    SizedBox(width: 16),
-                    Expanded(
-                      child: Text(
-                        'QR Thanh toan',
-                        textAlign: TextAlign.center,
+                      appBarSpace,
+                      IconImgButton(
+                        'chat_comment.webp',
+                        backgroundColor: iconBgrColor,
                       ),
-                    ),
-                    SizedBox(width: 16),
-                    Expanded(
-                      child: Text(
-                        'Vi tien ich',
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ],
+                      appBarPadding,
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ))
+            ));
 
-          // Background image Clipped
-          ..add(Positioned(
-            top: 0,
-            height: height - deltaExtent / 2,
-            width: constraints.maxWidth,
-            child: ClipRect(
-              clipper: RectClipper(minExtent),
-              child: imgBgr,
-            ),
-          ))
+            // App bar transform position
+            iconBgrColor = transformColor(const Color(0xff395241), null, t, 2);
+            final iconSize = transform(44, 32, t, 2);
+            final iconPadding = transform(8, 4, t, 2);
+            final double cardWidth = constraints.maxWidth - (cardMarginHorizontal * 2);
+            final cardSpace = (cardWidth - (IconImgButton.tapTargetSize * 4)) / 5;
 
-          // Splash transform color
-          ..add(Positioned(
-            left: 0,
-            right: 0,
-            bottom: 0,
-            top: 0,
-            child: Container(
-              height: currentExtent,
-              width: constraints.maxWidth,
-              color: transformColor(null, Colors.white, t, 3),
-            ),
-          ));
-
-        // App bar
-        const appBarPadding = SizedBox(width: 4);
-        final appBarContentWidth = constraints.maxWidth - (appBarPadding.width! * 2);
-        const totalIconImgButtonSize = IconImgButton.tapTargetSize * 7;
-        final appBarSpace = SizedBox(width: (appBarContentWidth - totalIconImgButtonSize) / 6);
-
-        //App bar fixed position
-        Color iconBgrColor = transformColor(Colors.black26, null, t);
-        children.add(Positioned(
-          left: 0,
-          top: 0,
-          right: 0,
-          child: Container(
-            height: minExtent,
-            color: transformColor(null, const Color(0xff395241), t, 2),
-            child: SafeArea(
-              bottom: false,
+            children.add(Positioned(
+              left: transform(
+                cardSpace + cardPadding,
+                appBarPadding.width! + IconImgButton.tapTargetSize + appBarSpace.width!,
+                t,
+                2,
+              ),
+              right: transform(
+                cardSpace + cardPadding,
+                appBarPadding.width! + IconImgButton.tapTargetSize * 2 + appBarSpace.width! * 2,
+                t,
+                2,
+              ),
+              top: constraints.maxHeight > maxExtent
+                  ? null
+                  : transform(minExtent + cardPadding, minExtent - IconImgButton.tapTargetSize - 4, t, 2),
+              bottom:
+                  constraints.maxHeight < maxExtent ? null : deltaExtent - IconImgButton.tapTargetSize - cardPadding,
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  appBarPadding,
                   IconImgButton(
-                    'search.webp',
+                    'momomain_money_in.webp',
+                    size: iconSize,
+                    padding: iconPadding,
+                    backgroundRadius: 16,
                     backgroundColor: iconBgrColor,
                   ),
-                  appBarSpace,
-                  const Spacer(),
-                  appBarSpace,
                   IconImgButton(
-                    'notifications_bell.webp',
+                    'momomain_withdraw.webp',
+                    size: iconSize,
+                    padding: iconPadding,
+                    backgroundRadius: 16,
                     backgroundColor: iconBgrColor,
                   ),
-                  appBarSpace,
                   IconImgButton(
-                    'chat_comment.webp',
+                    'navigation_qrcode.webp',
+                    size: iconSize,
+                    padding: iconPadding,
+                    backgroundRadius: 16,
                     backgroundColor: iconBgrColor,
                   ),
-                  appBarPadding,
+                  IconImgButton(
+                    'home_wallet_inactive.webp',
+                    size: iconSize,
+                    padding: iconPadding,
+                    backgroundRadius: 16,
+                    backgroundColor: iconBgrColor,
+                  ),
                 ],
               ),
-            ),
+            ));
+
+            return Stack(children: children);
+          },
+        ),
+        Positioned(
+          top: minExtent,
+          left: 0,
+          right: 0,
+          child: FloatingBalanceBar(isShowWhen: t == 1),
+        ),
+      ],
+    );
+  }
+}
+
+class SearchArea extends StatelessWidget {
+  const SearchArea({
+    super.key,
+    required this.appBarContentWidth,
+    required this.appBarSpace,
+    required this.iconBgrColor,
+  });
+
+  final double appBarContentWidth;
+  final SizedBox appBarSpace;
+  final Color iconBgrColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.centerLeft,
+      children: [
+        Container(
+          width: appBarContentWidth - IconImgButton.tapTargetSize * 2 - appBarSpace.width! * 3 - 4,
+          height: 32,
+          margin: const EdgeInsets.only(left: 4),
+          decoration: BoxDecoration(
+            color: iconBgrColor,
+            borderRadius: BorderRadius.circular(16),
           ),
-        ));
+        ),
+        const IconImgButton(
+          'search.webp',
+          backgroundColor: Colors.transparent,
+        ),
+      ],
+    );
+  }
+}
 
-        // App bar transform position
-        iconBgrColor = transformColor(const Color(0xff395241), null, t);
-        final iconSize = transform(48, 32, t);
-        final iconPadding = transform(8, 4, t);
+class FloatingBalanceBar extends StatelessWidget {
+  const FloatingBalanceBar({
+    super.key,
+    required this.isShowWhen,
+  });
 
-        children.add(Positioned(
-          height: minExtent,
-          left: transform(16, appBarPadding.width! + IconImgButton.tapTargetSize + appBarSpace.width!, t),
-          right: transform(16, appBarPadding.width! + IconImgButton.tapTargetSize * 2 + appBarSpace.width! * 2, t),
-          bottom: transform(boxPaddingBottom + actionFloatTextStyle.fontSize! * actionFloatTextStyle.height! * 2, 0, t),
-          child: SafeArea(
-            bottom: false,
-            minimum: EdgeInsets.symmetric(horizontal: transform(16, 0, t)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconImgButton(
-                  'momomain_money_in.webp',
-                  size: iconSize,
-                  padding: iconPadding,
-                  backgroundRadius: 16,
-                  backgroundColor: iconBgrColor,
-                ),
-                IconImgButton(
-                  'momomain_withdraw.webp',
-                  size: iconSize,
-                  padding: iconPadding,
-                  backgroundRadius: 16,
-                  backgroundColor: iconBgrColor,
-                ),
-                IconImgButton(
-                  'navigation_qrcode.webp',
-                  size: iconSize,
-                  padding: iconPadding,
-                  backgroundRadius: 16,
-                  backgroundColor: iconBgrColor,
-                ),
-                IconImgButton(
-                  'home_wallet_inactive.webp',
-                  size: iconSize,
-                  padding: iconPadding,
-                  backgroundRadius: 16,
-                  backgroundColor: iconBgrColor,
-                ),
-              ],
-            ),
-          ),
-        ));
+  final bool isShowWhen;
 
-        return Stack(children: children);
-      },
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 300),
+      child: isShowWhen
+          ? const BalanceBar(
+              contentPadding: EdgeInsets.symmetric(horizontal: 16),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(12),
+                  bottomRight: Radius.circular(12),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    offset: Offset(0, 4),
+                    blurRadius: 10,
+                    spreadRadius: -1,
+                  ),
+                  BoxShadow(
+                    color: Colors.black12,
+                    offset: Offset(0, 8),
+                    blurRadius: 20,
+                    spreadRadius: -2,
+                  ),
+                ],
+              ),
+            )
+          : const SizedBox.shrink(),
     );
   }
 }
@@ -346,49 +407,9 @@ class RectClipper extends CustomClipper<Rect> {
 
   @override
   Rect getClip(Size size) {
-    Rect rect = Rect.fromLTWH(0, 0, size.width, maxHeight);
-    return rect;
+    return Rect.fromLTWH(0, 0, size.width, maxHeight);
   }
 
   @override
-  bool shouldReclip(covariant CustomClipper<Rect> oldClipper) => false;
-}
-
-class IconImgButton extends StatelessWidget {
-  const IconImgButton(
-    this.name, {
-    super.key,
-    this.size = 32,
-    this.padding = 4,
-    this.backgroundColor = Colors.black26,
-    this.backgroundRadius,
-  });
-
-  final String name;
-  final double size;
-  final double padding;
-  final Color? backgroundColor;
-  final double? backgroundRadius;
-
-  static const double tapTargetSize = kMinInteractiveDimension;
-
-  @override
-  Widget build(BuildContext context) {
-    return IconButton(
-      icon: Image.asset('assets/icons/$name'),
-      padding: EdgeInsets.all(padding),
-      constraints: BoxConstraints(maxWidth: size, maxHeight: size),
-      style: IconButton.styleFrom(
-        // Expands the minimum tap target size to 48px by 48px.
-        tapTargetSize: MaterialTapTargetSize.padded,
-        backgroundColor: backgroundColor,
-        shape: backgroundRadius == null
-            ? null
-            : RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(backgroundRadius!),
-              ),
-      ),
-      onPressed: () {},
-    );
-  }
+  bool shouldReclip(RectClipper oldClipper) => oldClipper.maxHeight != maxHeight;
 }
